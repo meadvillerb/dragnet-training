1eccb93ac5c1a2bcbf926e3bebb86cc1b02e0179
Ian D. Eccles <ian.eccles@gmail.com>
2011-03-19 00:56:42 -0400

    don't need models directory, slap 'em all in dragnet-training.rb

--- dragnet-training.rb
+++ dragnet-training.rb
@@ -2,4 +2,27 @@
 
 require 'ohm'
 require 'typhoeus'
-require File.expand_path('../models/page', __FILE__)
+require 'classifier'
+
+class Page < Ohm::Model
+  attribute :name
+  attribute :html
+  list :blocks, BlockContent
+  
+  index :name
+  
+  def validate
+    assert_present :name
+  end
+  
+  def parse_block_content
+    self.blocks.clear
+  end
+end
+
+class BlockContent < Ohm::Model
+  attribute :element
+  attribute :id_name
+  attribute :class_name
+  attribute :content
+end
